392d97913c2f8aa5bd71d615a7aecbad208041c0
carsonic <cc4254a@american.edu>
2025-04-26 23:14:55 -0400

    Cleaning data

--- Final_Project.R
+++ Final_Project.R
@@ -1,5 +1,22 @@
 # Load Packages
-pacman::p_load(tidyverse, readxl)
+pacman::p_load(tidyverse, readxl, lubridate, janitor)
 
 # Read the first sheet
 faa_data <- read_excel("Public.xlsx")
+
+glimpse(faa_data)
+
+# Parse INCIDENT_DATE and LUPDATE and TIME as dates
+faa_data <- faa_data |> 
+  mutate(
+    INCIDENT_DATE = as_date(INCIDENT_DATE),
+    LUPDATE = as_date(LUPDATE),
+    TIME = lubridate::hm(TIME)
+  )
+
+# Clean names using janitor package
+faa_data <- faa_data |> 
+  janitor::clean_names()
+
+glimpse(faa_data)
+
