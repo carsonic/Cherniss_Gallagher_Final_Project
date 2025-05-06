7ed36b72e944964cee53d3279d0b6c13fb0e0a38
carsonic <cc4254a@american.edu>
2025-05-06 15:17:17 -0400

    Update shiny app

--- final_project_shiny_app/final_project_app.R
+++ final_project_shiny_app/final_project_app.R
@@ -3,34 +3,61 @@ pacman::p_load(tidyverse, readxl, lubridate, janitor, shiny, plotly, shinythemes
 # App setup
 ui <- navbarPage(
   title = "FAA Wildlife Strikes",
-  theme = shinythemes::shinytheme("darkly"),
+  theme = shinythemes::shinytheme("readable"),
   tabPanel("Overview",
            fluidPage(
              h3("Project Overview"),
              p("DATA-413 Final Project"),
              p("Carson Cherniss & Ainsley Gallagher"),
              h4("Research Question:"),
-             p("During what time of day are wildlife strikes most common?")
+             p("During what time of day are wildlife strikes by aircraft most common?"), 
+             h4("Dataset:"),
+             p("The dataset comes from the Federal Aviation Administration (FAA) Wildlife Strike Database. 
+             It contains detailed records of wildlife strikes involving civil aircraft in the United States. 
+             This analysis uses data from 1990 to 2023, with approximately 300,000 observations and variables including 
+               'incident date,' 'time of day,' 'species,' and 'location.'"), 
+             h4("Methods:"),
+             tags$ul(
+               tags$li("Exploratory Data Analysis: Visualizations of wildlife strikes by time of day and hour of day."),
+               tags$li("Seasonal Trends: Interactive plot allowing users to view monthly breakdowns."),
+               tags$li("Statistical Test: Chi-square test to evaluate whether wildlife strikes are evenly distributed across different times of day.")
+             )
            )
   ),
-  tabPanel("EDA: Time of Day",
+  tabPanel("Data Visualization: Time of Day",
            fluidPage(
              h3("Wildlife Strikes by Time of Day"),
-             plotlyOutput("timeOfDayPlot"), 
+             plotlyOutput("timeOfDayPlot"),
+             p("Based on this visualization, it appears that wildlife strikes are most common during the day. 
+               We investigate this further with statisitcal testing."),
+             p("Without a codebook, we do not know what hours of the day were classified as 'day', 'night', 'dawn' or 'dusk'. 
+               Thus, we can also visualize the distribution of wildlife strikes by hour of the day."),
              h3("Wildlife Strikes by Hour of Day"),
-             plotlyOutput("timePlot")
+             plotlyOutput("timePlot"),
+             p("Here, we can see that strikes appear most common between 7 and 11.
+               Wildlife strikes seem common between the hours of 7 and 23. We can investigate this further with statistical testing.")
            )
   ),
-  tabPanel("EDA: Monthly Trends",
+  tabPanel("Data Visualization: Monthly Trends",
            fluidPage(
              h3("Wildlife Strikes by Month"),
+             p("Data visualization has shown that wildlife strikes appear most common during the day.
+               Those visualizations are for all strikes throughout the year. However, things like migration habits of birds may cause this trend to change throughout the year.
+               With this visualization, we can see how trends change throughout the year."),
              selectInput("selected_month", "Select a Month:", choices = month.name),
-             plotlyOutput("monthlyPlot")
+             plotlyOutput("monthlyPlot"),
+             p("Here, we can see that the overall trend of 'day' having the most wildlife strikes holds true for most months.
+               However, October has an interesting discrepency, where 'night' has more strikes than 'day'.
+               This could be due to bird migrations."), 
+             p("The winter months (January, February and December) as well as the summer months (June, July and August) 
+               have the greatest difference between the amount of 'day' and 'night' strikes.
+               Whereas spring and fall months have a smaller difference between 'day' and 'night' strikes. This could be due to the migratory patterns of birds, 
+               as birds tend to migrate in the fall and the spring and may be more active in the skies during these times.")
            )
   ), 
   tabPanel("Statistical Test",
            fluidPage(
-             h2("Chi-Square Test Results")
+             h3("Chi-Square Test Results")
            )
   )
 )
